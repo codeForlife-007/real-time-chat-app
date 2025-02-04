4f48ccafd153050937b3447128da0fd224626a7f
code-help007 <kuldip.chauhan@itpl.io>
2025-02-04 16:32:51 +0530

    backend dynamic url

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.chat.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+  @Value("${backend.url}")
+  private String backendUrl;
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat")
-            .setAllowedOrigins("http://localhost:8080")
+            .setAllowedOrigins(backendUrl)
             .withSockJS();
   }
 
